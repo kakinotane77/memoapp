0d594f93302e62caaf7d0c9a2a423fea60100e7a
kakinotane77 <kakitsubo.yasuaki.77@gmail.com>
2025-01-18 09:20:43 +0900

    MySQLの接続方法を環境変数に移行

--- src/main/java/in/techcamp/memoapp/DotenvConfig.java
+++ src/main/java/in/techcamp/memoapp/DotenvConfig.java
@@ -1,22 +1,14 @@
 package in.techcamp.memoapp;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class DotenvConfig {
+    private static final Dotenv dotenv = Dotenv.configure()
+            .directory("./")
+            .ignoreIfMissing()
+            .load();
 
-    @PostConstruct
-    public void loadEnv() {
-        Dotenv dotenv = Dotenv.configure()
-                .directory(System.getProperty("user.dir")) // プロジェクトルートディレクトリを指定
-                .filename(".env")
-                .load();
-
-        // 環境変数をシステムプロパティとして設定
-        System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
-        System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
-        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+    public static String get(String key) {
+        return dotenv.get(key);
     }
 }
--- src/main/java/in/techcamp/memoapp/MemoAppApplication.java
+++ src/main/java/in/techcamp/memoapp/MemoAppApplication.java
@@ -5,10 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("in.techcamp.memoapp") // Mapperのパッケージを指定
+@MapperScan("in.techcamp.memoapp")
 public class MemoAppApplication {
 
 	public static void main(String[] args) {
+		// 環境変数を Spring Boot プロパティに設定
+		System.setProperty("MYSQL_URL", DotenvConfig.get("MYSQL_URL"));
+		System.setProperty("MYSQL_USERNAME", DotenvConfig.get("MYSQL_USERNAME"));
+		System.setProperty("MYSQL_PASSWORD", DotenvConfig.get("MYSQL_PASSWORD"));
+
+		// アプリケーション起動
 		SpringApplication.run(MemoAppApplication.class, args);
 	}
 }
